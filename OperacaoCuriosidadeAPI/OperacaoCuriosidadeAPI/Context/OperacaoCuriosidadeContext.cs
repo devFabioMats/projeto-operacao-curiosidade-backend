999eed98886294584e3bec3d8993ab94421ba8b6
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-13 14:41:46 -0300

    DbContext implementado

---
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Context/OperacaoCuriosidadeContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OperacaoCuriosidadeAPI.Entities;
+
+namespace OperacaoCuriosidadeAPI.Context
+{
+    public class OperacaoCuriosidadeContext : DbContext   // esse context faz a ligação com o banco de dados
+    {
+        // responsavel por passar a configurção de banco de dados da DbContext
+        public OperacaoCuriosidadeContext(DbContextOptions<OperacaoCuriosidadeContext> options) : base(options)
+        {
+        }
+        public DbSet<Colaborador> Colaboradores { get; set; }
+    }
+}
