c65d53ea7890ff8255978667a77fb3004a92e225
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-13 15:03:26 -0300

    Incrmeentando a controladora

---
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Controllers/ColaboradorController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using OperacaoCuriosidadeAPI.Context;
+
+namespace OperacaoCuriosidadeAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ColaboradorController : ControllerBase
+    {
+        // declarando um campo privado e somente leitura chamado _context do tipo OperacaoCuriosidadeContext.
+        private readonly OperacaoCuriosidadeContext _context;
+    }
+}
