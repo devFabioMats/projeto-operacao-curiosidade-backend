404529c648e17864fd7cbd234c697eac36220d13
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-15 19:15:05 -0300

    criando o modelo da dashboard

---
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Models/Dashboard.cs
@@ -0,0 +1,9 @@
+﻿namespace OperacaoCuriosidadeAPI.Models
+{
+    public class Dashboard
+    {
+        public int TotalCadastros { get; set; }
+        public int CadastrosInativos { get; set; }
+        public int CadastrosPendentes { get; set; }
+    }
+}
