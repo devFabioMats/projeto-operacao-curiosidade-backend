47b35d4df3e9281c17dc49975e09b5a6709b7549
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-16 11:41:16 -0300

    adicionando validação para email e senha do usuario

--- OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Models/Usuario.cs
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Models/Usuario.cs
@@ -1,10 +1,20 @@
-﻿namespace OperacaoCuriosidadeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OperacaoCuriosidadeAPI.Models
 {
     public class Usuario
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
         public string? Email { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
+        [MinLength(6, ErrorMessage = "A senha deve ter no mínimo 6 caracteres.")]
         public string? Senha { get; set; }
     }
 }
