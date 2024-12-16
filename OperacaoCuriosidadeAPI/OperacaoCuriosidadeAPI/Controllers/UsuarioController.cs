5264559eeb5dac2ec2d3c0bb360c4199c43e495f
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-16 01:10:39 -0300

    Add POST e GET by ID

--- OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Controllers/UsuarioController.cs
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Controllers/UsuarioController.cs
@@ -1,6 +1,40 @@
-﻿namespace OperacaoCuriosidadeAPI.Controllers
+﻿using Microsoft.AspNetCore.Mvc;
+using OperacaoCuriosidadeAPI.Context;
+using OperacaoCuriosidadeAPI.Models;
+
+namespace OperacaoCuriosidadeAPI.Controllers
 {
-    public class UsuarioController
+    [ApiController]
+    [Route("[controller]")]
+    public class ColaboradorController : ControllerBase
     {
+        // declarando um campo privado e somente leitura chamado _context do tipo OperacaoCuriosidadeContext.
+        private readonly OperacaoCuriosidadeContext _context;
+
+        public ColaboradorController(OperacaoCuriosidadeContext context)
+        {
+            _context = context;
+        }
+
+        // POST:  Colaborador
+        [HttpPost]
+        public IActionResult Create(Colaborador colaborador)
+        {
+            _context.Add(colaborador);
+            _context.SaveChanges();
+            return CreatedAtAction(nameof(ObterPorId), new { id = colaborador.Id }, colaborador);
+        }
+
+        // GET: Colaborador
+        [HttpGet("{id}")]
+        public IActionResult ObterPorId(int id)
+        {
+            var colaborador = _context.Colaboradores.Find(id);
+            if (colaborador == null)
+            {
+                return NotFound();
+            }
+            return Ok(colaborador);
+        }
     }
 }
