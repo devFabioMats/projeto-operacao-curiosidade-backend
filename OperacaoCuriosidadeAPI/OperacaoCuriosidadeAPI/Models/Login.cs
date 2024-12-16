b9d3694a91bdd12914f025528359d71aa8ca4e70
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-16 11:37:29 -0300

    criando o modelo para login do usuario

---
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Models/Login.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OperacaoCuriosidadeAPI.Models
+{
+    public class Login
+    {
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O campo Senha é obrigatório.")]
+        public string Senha { get; set; }
+    }
+}
