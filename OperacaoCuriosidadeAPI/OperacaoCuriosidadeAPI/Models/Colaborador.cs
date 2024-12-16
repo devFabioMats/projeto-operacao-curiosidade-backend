6fda342a34429c5c8baeaf12a1f6beca881cb146
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-16 11:21:16 -0300

    adicionado as validações do colaborador

--- OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Models/Colaborador.cs
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Models/Colaborador.cs
@@ -1,15 +1,34 @@
-﻿namespace OperacaoCuriosidadeAPI.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OperacaoCuriosidadeAPI.Models
 {
     public class Colaborador
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O campo Status é obrigatório.")]
         public bool Status { get; set; }
+
+        [Required(ErrorMessage = "O campo Nome é obrigatório.")]
         public string? Nome { get; set; }
+
+        [Required(ErrorMessage = "O campo Idade é obrigatório.")]
+        [Range(18, 100, ErrorMessage = "A idade deve estar entre 18 e 100 anos.")]
         public int Idade { get; set; }
+
+        [Required(ErrorMessage = "O campo Email é obrigatório.")]
+        [EmailAddress(ErrorMessage = "O campo Email deve ser um endereço de email válido.")]
         public string? Email { get; set; }
+
         public string? Endereco { get; set; }
+
+        [MaxLength(150, ErrorMessage = "O campo Interesses não pode exceder 150 caracteres.")]
         public string? Interesses { get; set; }
+
+        [MaxLength(150, ErrorMessage = "O campo Sentimentos não pode exceder 150 caracteres.")]
         public string? Sentimentos { get; set; }
+
+        [MaxLength(150, ErrorMessage = "O campo Valores não pode exceder 150 caracteres.")]
         public string? Valores { get; set; }
 
         public bool IsPendente
