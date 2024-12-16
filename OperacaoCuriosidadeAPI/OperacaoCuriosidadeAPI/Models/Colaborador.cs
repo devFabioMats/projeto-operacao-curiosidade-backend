512440d8ad791aa9c24452370d0ec6da6add1c03
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-15 23:55:06 -0300

    marcando as propriedades como anuláveis adicionando ?

--- OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Models/Colaborador.cs
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Models/Colaborador.cs
@@ -4,12 +4,12 @@
     {
         public int Id { get; set; }
         public bool Status { get; set; }
-        public string Nome { get; set; }
+        public string? Nome { get; set; }
         public int Idade { get; set; }
-        public string Email { get; set; }
-        public string Endereco { get; set; }
-        public string Interesses { get; set; }
-        public string Sentimentos { get; set; }
-        public string Valores { get; set; }
+        public string? Email { get; set; }
+        public string? Endereco { get; set; }
+        public string? Interesses { get; set; }
+        public string? Sentimentos { get; set; }
+        public string? Valores { get; set; }
     }
 }
