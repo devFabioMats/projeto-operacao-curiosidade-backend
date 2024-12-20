4c7422390c01ea85f5ba6fd7b52c084a347d975b
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-20 11:36:21 -0300

    criando o gerador de token

---
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Services/TokenService.cs
@@ -0,0 +1,33 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using OperacaoCuriosidadeAPI.Models;
+
+namespace OperacaoCuriosidadeAPI.Services
+{
+    public class TokenService
+    {
+        public static object GeradorToken(Usuario usuario)
+        {
+            var key = Encoding.ASCII.GetBytes(Key.Segredo);
+            var configuracaoToken = new SecurityTokenDescriptor
+            {
+                Subject = new System.Security.Claims.ClaimsIdentity(new Claim[]
+                {
+                    new Claim("idUsuario", usuario.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddHours(1),
+                SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(configuracaoToken);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            return new
+            {
+                token = tokenString
+            };
+        }
+    }
+}
