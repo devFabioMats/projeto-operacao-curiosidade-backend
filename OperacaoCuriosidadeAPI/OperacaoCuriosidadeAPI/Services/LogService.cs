002ad4dee88ad562ff17059bdc1dd657d1302c63
Fabio Eizo Matsumoto <fabioe.matsumoto@gmail.com>
2024-12-29 19:00:26 -0300

    add log

---
+++ OperacaoCuriosidadeAPI/OperacaoCuriosidadeAPI/Services/LogService.cs
@@ -0,0 +1,31 @@
+﻿namespace OperacaoCuriosidadeAPI.Logs
+{
+    public class Log
+    {
+        public void LogParaArquivo(string titulo, string mensagemLog)
+        {
+            string nomeArquivo = DateTime.Now.ToString("ddMMyyyy") + ".txt";
+
+            StreamWriter swLog;
+
+            if (File.Exists(nomeArquivo))
+            {
+                swLog = File.AppendText(nomeArquivo);
+            }
+            else
+            {
+                swLog = new StreamWriter(nomeArquivo);
+            }
+
+            swLog.WriteLine("Log:");
+            swLog.WriteLine($"{DateTime.Now.ToLongDateString()} {DateTime.Now.ToLongTimeString()}");
+
+            swLog.WriteLine($"Título da Mensagem: {titulo}");
+            swLog.WriteLine($"Mensagem: {mensagemLog}");
+
+            swLog.WriteLine("-------------------------------");
+            swLog.WriteLine("");
+            swLog.Close();
+        }
+    }
+}
